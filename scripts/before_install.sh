90fabdecd610c4c2f026ae581c08a608c4c870f4
itsanandraghavan <itsanandraghavan@gmail.com>
2024-04-12 19:14:51 +0530

    build error fixes

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,5 +4,5 @@ curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt update
 sudo apt install -y nodejs
-sudo npm install -y npm
+sudo apt install -y npm
 sudo npm install -g pm2
